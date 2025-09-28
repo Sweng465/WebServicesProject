b272e1fccf4145c88ed8f7027ee6a22a58c6d320
Konrad Ruckdeschel <konradruck@gmail.com>
2025-09-28 15:52:26 -0400

    Buyer get and post

--- server.js
+++ server.js
@@ -39,6 +39,44 @@ app.post("/api/cars", (req, res) => {
     res.status(201).json(newCar);
 })
 
+
+// BUYER
+// simple array
+let buyers = [
+    { id: 1, username: 'dave123', icon: 'IMG_1.png'},
+    { id: 2, username: 'john34', icon: 'IMG_2.png'},
+    { id: 3, username: 'john1976', icon: 'IMG_3.png'}
+];
+
+// GET enpoint to get all buyers
+app.get("/api/buyers", (req, res) => {
+    res.json(buyers);
+})
+
+// POST endpoint to add a new buyer account
+app.post("/api/buyers", (req, res) => {
+    const { username, icon } = req.body;
+
+    // basic validation
+    if (!username || !icon){
+        return res.status(400).json({
+            error: "Invalid buyer account data."
+        });
+    }
+
+    // create new buyer object
+    const newBuyer = {
+        id: buyers.length + 1,
+        username,
+        icon,
+    };
+
+    // add to array and return
+    buyers.push(newBuyer);
+    res.status(201).json(newBuyer);
+})
+
+
 // start server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
