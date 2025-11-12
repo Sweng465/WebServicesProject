c4a5c92856778efa8e68c62591270a3492719a2e
konradRuck <konradruck@gmail.com>
2025-11-12 11:09:35 -0500

    Button color change.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -32,20 +32,20 @@ const Profile = () => {
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-orange-600 to-blue-600 flex flex-col items-center">
       <Header />
-      <div className="max-w-3xl min-w-100 mx-auto p-6 bg-white rounded-lg mt-6 flex flex-col items-center text-center">
+      <div className="max-w-3xl min-w-100 mx-auto p-6 bg-white rounded-lg shadow-lg mt-6 flex flex-col items-center text-center">
         <h1 className="text-3xl font-bold mb-4"> Profile </h1>
         {/* Update to get user's icon from database (replace w/ default if null) */}
         <img
           src={defaultIcon}
           alt="User profile picture"
-          className="w-[250px] h-[250px] object-cover rounded-full mb-4"
+          className="w-[250px] h-[250px] object-cover rounded-full mb-4 shadow-md"
         />
 
         <h2 className="text-2xl font-bold mb-4"> {profile.username} </h2>
-        <h2 className="text-xl mb-4"> {profile.email} </h2>
+        <h2 className="text-xl font-medium mb-4"> {profile.email} </h2>
 
         {/* Update to direct to page where users can edit their account details */}
-        <button className= "p-2 px-8 font-medium rounded-md transition bg-gray-300 hover:bg-gray-200">
+        <button className= "p-2 px-8 mb-4 font-medium text-white rounded-md shadow-md transition bg-blue-700 hover:bg-blue-800">
           Manage Account
         </button>
 
